58f2c467de893ad413edba5d412ebbfddfa1d134
WinterJackson <winterjacksonwj@gmail.com>
2023-08-16 23:12:14 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print ("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name): 
+    print(f"Hello, {name}!")
+greet ("Noreen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    print (num1 + num2)
 
 def halve(number):
-    pass
+    if isinstance (number, (int, float)):
+        return (None)
+    
+    return number / 2
+
+def test_add (num1, num2):
+    assert(add(num1, num2) == 100)
+
+test_add (45, 55)
